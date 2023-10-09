f3d1b8a90d62ee486de7e05bac736a5ef71c4abd
dungcvvc <dung.can@netlink.vn>
2023-10-09 15:18:02 +0700

    fix_video

--- application.js
+++ application.js
@@ -5,9 +5,11 @@ var wrapperDiv;
 var duration_ad = 15000;
 var adTagAdsense =
   "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video_image&client=ca-video-pub-3166493188367342&description_url=http%3A%2F%2Fwww.tinmoi.vn&videoad_start_delay=0&hl=en&max_ad_duration=15000&sdmax=30000";
-var Application = function (src_video) {
+var Application = function () {
   wrapperDiv = document.getElementById("content_video");
-  getVideoDuration(src_video)
+  var sourceElement = wrapperDiv.getElementsByTagName("source")[0];
+  var videoSrc = sourceElement.getAttribute("src");
+  getVideoDuration(videoSrc)
     .then(function (duration) {
       if (duration <= 5) {
         adTagAdsense =
@@ -74,17 +76,17 @@ var Application = function (src_video) {
     player = videojs("content_video", vjsOptions);
     var imaOptions = {
       id: "content_video",
-      adTagUrl: adTagAdsense,
-        // "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&" +
-        // "iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&" +
-        // "impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&" +
-        // "cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js" +
-        // "&cmsid=496&vid=short_onecue&correlator=",
+      adTagUrl:
+        // adTagAdsense,
+        "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&" +
+        "iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&" +
+        "impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&" +
+        "cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js" +
+        "&cmsid=496&vid=short_onecue&correlator=",
       // 'https://googleads.g.doubleclick.net/pagead/ads?ad_type=video_text_image&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=30000&adtest=on'
       // "https://pubads.g.doubleclick.net/gampad/ads?iu=/22486823495/video_instream&description_url=https%3A%2F%2Fnetlink.vn%2F&tfcd=0&npa=0&sz=640x480&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=",
       // "https://pubads.g.doubleclick.net/gampad/ads?iu=/93656639,52958642/outstream_video_OO&description_url=https%3A%2F%2Fnetlink.vn%2F&tfcd=0&npa=0&sz=300x250%7C640x480&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator="
     };
-
 
     player.ima(imaOptions);
     // player.ima.requestAds();
@@ -97,10 +99,8 @@ var Application = function (src_video) {
       ) {
         startEvent = "touchend";
       }
-
       wrapperDiv.addEventListener(startEvent, initAdDisplayContainer);
     }
-    
   }
   function checkDivInViewableArea(div) {
     // Lấy vị trí của div
